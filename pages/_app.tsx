b45e68a67db27e2b51f0b6b577f5cded9b025d19
moi23 <moisesboas@gmail.com>
2021-05-21 09:28:31 -0300

    feat: configurated the project and added globalStyles

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { GlobalStyle } from '../styles/global';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </div>
+  );
+};
+
+export default App;
